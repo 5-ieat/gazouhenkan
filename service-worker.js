bb6e500a18caf6a5a0b76625d0b98a4c55b6a39a
5-ieat <eieieitadoraemon@gmail.com>
2026-05-21 19:42:13 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,4 @@
-// 最低限のPWA認識用おまじない（空っぽでもOKですがイベントだけ登録）
-self.addEventListener('fetch', function(event) {
-    const urlsToCache = [
+const urlsToCache = [
     './',              // トップページ（index.htmlを指します）
     './index.html',
     './manifest.json',
@@ -8,35 +6,3 @@ self.addEventListener('fetch', function(event) {
     './ga.png'
 
 ];
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-// アクティベート時に古いキャッシュを削除
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
-});
-
-// フェッチ要求（ネットワークが繋がらない時はキャッシュを返す）
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
-});
