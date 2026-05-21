90cdf84d868b6526bc45d874bb4180675bc95256
5-ieat <eieieitadoraemon@gmail.com>
2026-05-21 18:57:55 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -8,4 +8,35 @@ self.addEventListener('fetch', function(event) {
     './ga.ico'
 
 ];
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// アクティベート時に古いキャッシュを削除
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// フェッチ要求（ネットワークが繋がらない時はキャッシュを返す）
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
 });
